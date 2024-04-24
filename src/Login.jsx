4c7e70d9cde008ee5618c3bb9f347b736d7aa361
Pyrite32 <patrick@method93.com>
2024-04-24 10:00:56 -0400

    modified login+signup

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,6 +3,7 @@ import './Signup.css';
 import { Link } from "react-router-dom";
 import axios from 'axios'
 import { useNavigate } from "react-router-dom";
+import CompartmentsLogo from "./assets/compartments-logo-color.png"
 
 function Login() {
     const [email, setEmail] = useState()
@@ -11,60 +12,62 @@ function Login() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        axios.post('http://localhost:3001/login', {email, password})
-        .then(result => {console.log(result)
-            if(result.data === "Success"){
-                navigate('/Homepage')
-            }
-        })
-        .catch(err=> console.log(err))
+        axios.post('http://localhost:3001/login', { email, password })
+            .then(result => {
+                console.log(result)
+                if (result.data === "Success") {
+                    navigate('/Homepage')
+                }
+            })
+            .catch(err => console.log(err))
     }
 
     return (
-        <div className="d-flex justify-content-center align-items-center blue-background">
-            <div className="bg-white p-3 rounded w-40 ">
-                <h1>Compartments</h1>
-                <h2>Login</h2>
+        <div className="min-h-screen flex justify-end bg-gradient-to-r from-purple-400 to-red-400">
+            <img src={CompartmentsLogo} className="w-auto h-20 absolute top-5 left-5"></img>
+            <div className="bg-slate-200 m-24 pt-10 pl-10 pr-10 pb-20 max-w-xl">
+                <div className="bg-white p-3 rounded w-40 ">
+                    <h1>Compartments</h1>
+                    <h2>Login</h2>
 
-                <form onSubmit={handleSubmit}>
-         
-                    {/* Email Field */}
-                    <div className="mb-3">
-                        <label htmlFor="email">
-                            <strong>Email</strong>
-                        </label>
-                        <input
-                            type="email"
-                            placeholder="Enter Email"
-                            autoComplete="off"
-                            name="email"
-                            className="form-control rounded-0"
-                            onChange={(e) => setEmail(e.target.value)}
+                    <form onSubmit={handleSubmit}>
 
-                        />
-                    </div>
+                        {/* Email Field */}
+                        <div className="mb-3">
+                            <label htmlFor="email">
+                                <strong>Email</strong>
+                            </label>
+                            <input
+                                type="email"
+                                placeholder="Enter Email"
+                                autoComplete="off"
+                                name="email"
+                                className="form-control rounded-0"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
 
-                    {/* Password Field */}
-                    <div className="mb-3">
-                        <label htmlFor="password">
-                            <strong>Password</strong>
-                        </label>
-                        <input
-                            type="password"
-                            placeholder="Enter Password"
-                            name="password"
-                            className="form-control rounded-0"
-                            onChange={(e) => setPassword(e.target.value)}
+                        {/* Password Field */}
+                        <div className="mb-3">
+                            <label htmlFor="password">
+                                <strong>Password</strong>
+                            </label>
+                            <input
+                                type="password"
+                                placeholder="Enter Password"
+                                name="password"
+                                className="form-control rounded-0"
+                                onChange={(e) => setPassword(e.target.value)}
 
-                        />
-                    </div>
+                            />
+                        </div>
 
-                    <button type="submit" className="btn btn-success w-100 rounded-0">
-                        Login
-                    </button>
-                </form>
+                        <button type="submit" className="btn btn-success w-100 rounded-0">
+                            Login
+                        </button>
+                    </form>
 
-              
+                </div>
             </div>
         </div>
     );
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -3,6 +3,7 @@ import './Signup.css';
 import { Link } from "react-router-dom";
 import axios from 'axios'
 import { useNavigate } from "react-router-dom";
+import CompartmentsLogo from "./assets/compartments-logo-color.png"
 
 
 function Signup() {
@@ -21,29 +22,31 @@ function Signup() {
     }
 
     return (
+        <>
             <div className="min-h-screen flex justify-end bg-gradient-to-r from-purple-400 to-red-400">
+                <img src={CompartmentsLogo} className="w-auto h-20 absolute top-5 left-5"></img>
                 <div className="bg-slate-200 m-24 pt-10 pl-10 pr-10 pb-20 max-w-xl">
                     <h1 className="font-noto font-bold text-4xl mb-3">Your productivity starts here.</h1>
                     <p className="font-sans mb-4 italic text-slate-600">With Compartments, you're able to focus solely on tasks, and nothing else! We make it extra easy for you to manage your tasks on the web!</p>
                     <form onSubmit={handleSubmit}>
                         {/* Name Field */}
-                        <div className="">
-                            <label htmlFor="name" className="signup-label">
-                                Name
+                        <div className="p-2">
+                            <label htmlFor="name" className="signup-label pr-11">
+                            Name
                             </label>
                             <input
                                 type="text"
                                 placeholder=""
                                 autoComplete="off"
                                 name="name"
-                                className="form-control rounded-0 italic"
+                                className="form-control rounded-0 italic p-1 rounded-sm"
                                 onChange={(e) => setName(e.target.value)}
                             />
                         </div>
 
                         {/* Email Field */}
-                        <div className="">
-                            <label htmlFor="email" className="signup-label">
+                        <div className="p-2">
+                            <label htmlFor="email" className="signup-label pr-12">
                                 Email
                             </label>
                             <input
@@ -51,39 +54,36 @@ function Signup() {
                                 placeholder=""
                                 autoComplete="off"
                                 name="email"
-                                className="form-control rounded-0 font-noto italic"
+                                className="form-control rounded-1 italic p-1 rounded-sm"
                                 onChange={(e) => setEmail(e.target.value)}
 
                             />
                         </div>
 
                         {/* Password Field */}
-                        <div className="mb-20">
-                            <label htmlFor="password" className="signup-label">
+                        <div className="p-2 mb-5">
+                            <label htmlFor="password" className="signup-label pr-4">
                                 Password
                             </label>
                             <input
                                 type="password"
                                 placeholder=""
                                 name="password"
-                                className="form-control rounded-0 italic"
+                                className="form-control rounded-1 italic p-1"
                                 onChange={(e) => setPassword(e.target.value)}
-
                             />
                         </div>
-
-                        <button type="submit" className="btn btn-success button-register w-100 border rounded-0 border-color-blue-500">
-                            Register
+                        <button type="submit" className="btn bg-purple-400 p-2 px-4 font-extrabold text-xl border rounded-sm text-white">
+                            REGISTER
                         </button>
                     </form>
-
-                    <p className="italic text-slate-400 mt-3">Already have an Account?</p>
-
-                    <Link to="/login" className="btn btn-default border w-100 bg-light rounded-0 text-description-none">
-                        Login
+                    <p className="italic text-slate-400 mt-16 mb-6">Already have an Account?</p>
+                    <Link to="/login" className="btn bg-purple-400 p-2 px-4 font-extrabold text-xl border rounded-sm text-white">
+                        LOGIN
                     </Link>
                 </div>
             </div>
+        </>
     );
 }
 
